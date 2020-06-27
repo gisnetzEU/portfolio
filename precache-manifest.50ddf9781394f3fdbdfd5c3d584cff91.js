a49634eb9cf918c5ede944dcce4275f3d49ce216
gisnetz <gisnetz@msn.com>
2020-06-28 01:02:02 +0200

    Updates

---
+++ precache-manifest.50ddf9781394f3fdbdfd5c3d584cff91.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fe279b953c2aaae02549469c10d07de",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "23cc0d4ff7c2ea991c4f",
+    "url": "/portfolio/static/css/main.57aadb5c.chunk.css"
+  },
+  {
+    "revision": "4565d8c2fda90d7b8535",
+    "url": "/portfolio/static/js/2.1319b7f6.chunk.js"
+  },
+  {
+    "revision": "71772b085033e92f89cdfb3071333458",
+    "url": "/portfolio/static/js/2.1319b7f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23cc0d4ff7c2ea991c4f",
+    "url": "/portfolio/static/js/main.f2913a18.chunk.js"
+  },
+  {
+    "revision": "ab70fc6043c66e6fde87",
+    "url": "/portfolio/static/js/runtime-main.76ccea34.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/portfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "d6d4086fb1cade58b3f786620e17c3ef",
+    "url": "/portfolio/static/media/contactMail.d6d4086f.png"
+  },
+  {
+    "revision": "9bd5573096ce87d12c1dad47de6861ff",
+    "url": "/portfolio/static/media/developerActivity.9bd55730.png"
+  },
+  {
+    "revision": "5b012ee1da52440b1f968fd688ae19cd",
+    "url": "/portfolio/static/media/gisnetz.5b012ee1.jpg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/portfolio/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "daab637051cfb1bd164e4899b13f0a7a",
+    "url": "/portfolio/static/media/proficiency.daab6370.svg"
+  },
+  {
+    "revision": "de3da8064f2e9ffab945772c2c67e46f",
+    "url": "/portfolio/static/media/talksCardBack.de3da806.svg"
+  }
+]);
